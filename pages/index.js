3c8071590ecb3d224c3af0088e8b71adadb24fb3
Drew hoffer <drew_hoffer@hotmail.com>
2020-04-09 22:11:14 -0400

    Grabbing products from db

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import axios from 'axios';
+import ProductList from '../components/Index/ProductList';
+import baseUrl from '../utils/baseUrl';
 
-function Home() {
-  React.useEffect( () => {
-    getProducts();
-  }, []);
+function Home({ products }) {
+  return <ProductList products = {products}/>;
+}
 
 
-  async function getProducts() {
-    const url = "http://localhost:3000/api/products";
-    const response = await axios.get(url);
-    console.log(response.data)
-  }
-  return <>home</>;
+Home.getInitialProps = async () => {
+  const url = `${baseUrl}/api/products`;
+  const response = await axios.get(url);
+  return {products:response.data}
+  
 }
 
 export default Home;
