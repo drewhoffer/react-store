ee9ea4d55111ff06f67dea36708dd24142ede11b
Drew hoffer <drew_hoffer@hotmail.com>
2020-05-02 13:26:50 -0400

    Found the bug!

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === "production" ? 'https://hoffer-ikea-clone.now.sh' : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === "production" ? 'https://hoffer-ikea-clone.herokuapp.com' : 'http://localhost:3000';
 
 export default baseUrl;
